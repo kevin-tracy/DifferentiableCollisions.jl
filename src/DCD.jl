2518e4866833edfb0a236ce871595537cc358176
Kevin Tracy <kevt.laser@gmail.com>
2022-08-22 11:58:00 -0400

    moved solver stuff to `src/solvers`

--- src/DCD.jl
+++ src/DCD.jl
@@ -9,20 +9,28 @@ import Polyhedra
 import MeshCat as mc
 import ForwardDiff
 
-include("coneqp/nt_scaling_chol.jl")
-include("coneqp/nt_scaling_chol_2.jl")
-include("coneqp/soc_utils.jl")
-include("coneqp/static_solver.jl")
-include("coneqp/static_solver2.jl")
-include("lp_solver.jl")
+# solver stuff
+include("solvers/coneqp/NT_scaling_chol.jl")
+include("solvers/coneqp/NT_scaling_chol_2.jl")
+include("solvers/coneqp/soc_utils.jl")
+include("solvers/coneqp/static_solver.jl")
+include("solvers/coneqp/static_solver2.jl")
+include("solvers/lp_solver.jl")
+
+# primitives
 include("primitives.jl")
 include("primitives_mrp.jl")
+include("mass_properties.jl")
+
+# visualizer
 include("visualizer.jl")
+
+# proximity stuff
 include("problem_matrices.jl")
 include("combine_problem_matrices.jl")
 include("proximity.jl")
 include("proximity_polytopes.jl")
-include("mass_properties.jl")
+
 
 export *, \
 
